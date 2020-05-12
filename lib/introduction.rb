0e735fcc433c57d2e7283fe8376c08b7b4a4ce0c
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-12 20:36:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,9 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
@@ -96,3 +98,4 @@ end
 #def introduction_with_language_optional(name, language="Ruby")
 #  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 #end
+
